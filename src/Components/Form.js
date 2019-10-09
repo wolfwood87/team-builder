c1367e403eb224ffe9694f9836ea11a980d1f70e
Derek <legato_5@yahoo.com>
2019-10-08 18:44:51 -0700

    started constructing form

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,5 +1,38 @@
 import React, { useState } from "react";
 
 const Form = props => {
-    
+    const [member, setMember] = useState({ name: '', email: '', role: ''});
+
+    const changedItem = e => {
+        setMember({ ...member, [e.target.name]: e.target.value });
+    };
+
+    return(
+        <form>
+            <label htmlFor='name'>Name</label>
+            <input
+                id="name"
+                name="name"
+                value={member.name}
+                type="text"
+                onChange={changedItem}
+            />
+            <label htmlFor='email'>Email</label>
+            <input
+                id="email"
+                name="email"
+                value={member.email}
+                type="text"
+                onChange={changedItem}
+            />
+            <label htmlFor='role'>Name</label>
+            <input
+                id="role"
+                name="role"
+                value={member.role}
+                type="text"
+                onChange={changedItem}
+            />
+        </form>
+    )
 }
